da54949724c47cba42ccfeb050b3e3cd18de9039
Evan Greenberg <greenberg@169-231-220-120.wireless.ucsb.edu>
2023-04-04 09:18:29 -0700

    update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-river='Ucayali_Requena'
-poly="/Volumes/Greenberg/Moility/GalleazziData/shapes/$river.gpkg"
-metrics="dswe"
+river='Jurua_Gaviao'
+poly="/Volumes/Greenberg/Moility/Comparative/SingleRivers/Jurua_Gaviao/$river.gpkg"
+metrics="single"
 # metrics="single"
 # out="/Users/greenberg/Documents/PHD/Writing/MobilityMethods/Figures/PubFigures/Figure6_Avulsion/$river"
-out="/Volumes/Greenberg/Moility/GalleazziData/RiverData/$river"
+out="/Volumes/Greenberg/Moility/Comparative/SingleRivers/Jurua_Gaviao/WaterLevel1"
 
 python ../CalculateMobility/main.py --poly $poly --metrics $metrics --out $out --river $river
 
-for value in 1 2 3 4
-do
-    echo $value
-    gif_root="$out/WaterLevel$value/mask"
-    gif_out="$out/WaterLevel$value.gif"
-    python make_gif.py --root $gif_root --out $gif_out
-done
